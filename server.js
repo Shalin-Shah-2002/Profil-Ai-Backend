097b8afaea1c597131ef1b02380232588c1db71b
Shalin-Shah-2002 <2002shalin@gmail.com>
2025-05-25 17:26:35 +0530

    add CORS support for production environment

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors({ origin: "https://profilai-lac.vercel.app", credentials: true }))
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }))
 app.use(passport.initialize())
 
